630f7d59e81419288ca75d07f2978273cadd9cf1
Rolf Gilet <rolf.gilet@ggainc.com>
2019-07-24 16:03:32 -0400

    Add project files.

---
+++ BooksApi/Services/BookService.cs
@@ -0,0 +1,25 @@
+﻿using BooksApi.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() =>
+            _books.Find(book => true).ToList();
+
+
+    }
+}
